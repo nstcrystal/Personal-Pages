ff1b687ff69dc0fdee84e06bf154ef62300990e9
Duypheloi <nstcrystal@gmail.com>
2025-11-30 14:01:54 +0700

    add secret-key for api

--- api/send.js
+++ api/send.js
@@ -1,11 +1,20 @@
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     if (req.method !== "POST") {
-        return res.status(405).json({ error: "Only POST allowed" });
+        return res.status(405).json({ error: "Why is it wrong again?" });
     }
 
     const WEBHOOK = process.env.WEBHOOK_URL;
+    const SECRET = process.env.API_SECRET;
     const { name, question } = req.body;
     const time = new Date().toLocaleString("vi-VN");
+
+    if (req.body.birthday !== SECRET) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
 
 
     const payload = {
@@ -24,22 +33,3 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ success: true });
 }
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
-
-//   const webhookURL = process.env.WEBHOOK_URL;
-
-//   try {
-//     await fetch(webhookURL, {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify(req.body)
-//     });
-
-//     res.status(200).json({ success: true });
-//   } catch (err) {
-//     res.status(500).json({ error: "Failed to send message" });
-//   }
-// }
